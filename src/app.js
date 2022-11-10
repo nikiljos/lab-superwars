2d1c253f99f71fc3b219890d8d156690abe43b9d
Nikhil Jose <nikhiljose12345@gmail.com>
2022-11-10 22:54:39 +0530

    implement stage 2

--- src/app.js
+++ src/app.js
@@ -26,6 +26,19 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    players.forEach((player,i)=>{
+        let type="hero"
+        if(i%2!=0){
+            type="villain"
+        }
+        let data = {
+            name: player,
+            strength: getRandomStrength(),
+            image: `images/super-${i+1}.png`,
+            type,
+        };
+        detailedPlayers.push(data)
+    })
 
     return detailedPlayers;
 }
@@ -34,6 +47,7 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.ceil(Math.random() * 100);
 }
 
 const buildPlayers = (players, type) => {
@@ -42,6 +56,18 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    players.forEach(playerData=>{
+        if (playerData.type != type) {
+            return;
+        }
+        fragment += `
+        <div class="player">
+            <img src="${playerData.image}" alt="">
+            <div class="name">${playerData.name}</div>
+            <div class="strength">${playerData.strength}</div>
+        </div>
+        `;
+    })
 
     return fragment;
 }
